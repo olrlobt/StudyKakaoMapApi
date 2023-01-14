327bf1615d82e58479936ca78f50dc1335eeea53
olrlobt <317tmdgjs@naver.com>
2023-01-14 19:10:07 +0900

    feat: 기본설정

---
+++ src/main/java/com/study/home/StudyKakaoMapApiApplication.java
@@ -0,0 +1,13 @@
+package com.study.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyKakaoMapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyKakaoMapApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/home/StudyKakaoMapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyKakaoMapApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
